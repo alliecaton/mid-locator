764df23250ba394e01f31d574afa2af3c9452089
Allie <alliecaton3@gmail.com>
2021-08-09 13:33:21 -0400

    first

---
+++ src/components/LocationInput.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div>
+			<label>Point A</label>
+			<input type="text" v-model="pointA" />
+		</div>
+		<div>
+			<label>Point B</label>
+			<input type="text" v-model="pointB" />
+		</div>
+		<div>
+			<label>Type</label>
+			<input type="text" v-model="type" />
+		</div>
+		<button>SEARCH</button>
+
+		<div>{{ pointA }}</div>
+		<div>{{ pointB }}</div>
+		<div>{{ type }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'location-input',
+
+	data() {
+		return {
+			pointA: '',
+			pointB: '',
+			type: '',
+		}
+	},
+}
+</script>
